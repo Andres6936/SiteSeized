b1ad574875d8bd473c0bcedff41a36ca30d843cf
Andres6936 <andres6936@gmail.com>
2025-01-03 22:42:42 -0500

    Refactor CSS handling by modifying HTML and dependencies.
    Replaces external CSS script with inlined CSS within a `<style>` tag in `index.html`. Updates `package.json` to include necessary HTML parsing and DOM manipulation libraries (`htmlparser2`, `domutils`, `domhandler`, and `dom-serializer`). Ensures cleaner integration of CSS rendering into the HTML structure.

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
 import {render} from "@master/css/render";
 import { StyleSheet } from '@master/css';
+import * as htmlparser from 'htmlparser2'
+import * as domutils from 'domutils';
+import * as renderdom from "dom-serializer";
+import * as domhandler from 'domhandler';
 
 (async () => {
     const html = await Bun.file("index.html").text();
+    const dom = htmlparser.parseDocument(html);
+
+    const [head] = domutils.getElementsByTagName("head", dom)
+    for (let element of head.children) {
+        const elementType = domutils.getName(element)
+        if (elementType === "script") {
+            const src = domutils.getAttributeValue(element, "src")
+            if (src === "https://cdn.master.co/css") {
+                domutils.removeElement(element)
+            }
+        }
+    }
+
     const {css} = render(html, {StyleSheet})
-    await Bun.write("index.css", css);
+    domutils.appendChild(head, new domhandler.Element("style", {}, [
+        new domhandler.Text(css)
+    ]))
+
+    await Bun.write("index.html", renderdom.default(dom));
 })();
